6a8e5577f67523e41b434b6f721a549ca250793b
Wojtek <105372499+imnotwojtek@users.noreply.github.com>
2023-09-03 08:00:02 +0200

    Update EP-update-install.sh

--- EP-update-install.sh
+++ EP-update-install.sh
@@ -1,45 +1,20 @@
 #!/bin/bash
 
-# Utworzenie pliku wymiany
-sudo fallocate -l 5G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
+# Utworzenie i zamontowanie pliku wymiany
+sudo fallocate -l 5G /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 
-# Konfiguracja parametrów systemowych
-echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
-echo 'vm.vfs_cache_pressure=50' | sudo tee -a /etc/sysctl.conf
-sudo sysctl -p
-
-# Zamontowanie pliku wymiany
-sudo swapon -a
+# Konfiguracja parametrów systemowych i aktualizacja
+echo -e 'vm.swappiness=10\nvm.vfs_cache_pressure=50' | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 
 # Aktualizacja serwerów DNS
-echo "nameserver 1.1.1.1" | sudo tee /etc/resolv.conf
-echo "nameserver 1.0.0.1" | sudo tee -a /etc/resolv.conf
+echo -e "nameserver 1.1.1.1\nnameserver 1.0.0.1" | sudo tee /etc/resolv.conf
 
 # Dodanie informacji o stanie systemu do pliku .bashrc
-echo 'cpu_usage=$(top -bn1 | grep load | awk "{print \$3}")
-ram_usage=$(free | awk "/Mem:/ { print \$3/\$2 * 100.0 }")
-disk_usage=$(df -h / | awk "/\// {print \$(NF-1)}")
-external_ip=$(curl -s https://checkip.amazonaws.com)
-internal_ip=$(hostname -I | awk "{print \$1}")
-echo ""
-echo "===== System Status ====="
-echo "CPU Usage: \$cpu_usage%"
-echo "RAM Usage: \$ram_usage%"
-echo "Disk Usage: \$disk_usage"
-echo "External IP: \$external_ip"
-echo "Internal IP: \$internal_ip"
-echo "========================="
-echo ""' >> ~/.bashrc
+echo -e '\ncpu_usage=$(top -bn1 | grep load | awk "{print \$3}")\nram_usage=$(free | awk "/Mem:/ { print \$3/\$2 * 100.0 }")\ndisk_usage=$(df -h / | awk "/\// {print \$(NF-1)})\nexternal_ip=$(curl -s https://checkip.amazonaws.com)\ninternal_ip=$(hostname -I | awk "{print \$1}")\n\n===== System Status =====\nCPU Usage: \$cpu_usage%\nRAM Usage: \$ram_usage%\nDisk Usage: \$disk_usage\nExternal IP: \$external_ip\nInternal IP: \$internal_ip\n=========================\n' >> ~/.bashrc
 
-# Aktualizacja systemu
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt autoremove -y
-sudo apt clean
-sudo systemctl restart systemd-journald
+# Aktualizacja i czyszczenie systemu
+sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt clean && sudo systemctl restart systemd-journald
 
 # Instalacja EasyPanel
 curl -sSL https://get.easypanel.io | sh
